0a312e218d4ac9c2384cc0d0e2ffce5b9c8dcb82
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-19 09:28:14 +0800

    Create A boring code.cpp

---
+++ A boring code.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("It's always important to forgive oneself.\n");
+	printf("人生很长，要学会与自己和解。\n");
+	printf("\n");
+	system("pause");
+	return 0;
+}
