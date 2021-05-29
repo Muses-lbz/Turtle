61794ab4ef17ac53b5be6ceeac65a5618c232d22
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-29 18:08:43 +0800

    Update A boring code.cpp

--- A boring code.cpp
+++ A boring code.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	printf("I like the simple pleasures in life.\n我喜欢生活中那些简单的乐趣。\n");
+	printf("Life's too short to be pissed off all the time.\n生命太短暂，不能总是愤世嫉俗。\n");
 	printf("\n");
 	system("pause");
 	return 0;
