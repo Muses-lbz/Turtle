df22db9b0f3cc6bdb5f8366ddf69be481ecd93f2
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-09-09 22:56:07 +0800

    Update A boring code.cpp

--- A boring code.cpp
+++ A boring code.cpp
@@ -3,7 +3,17 @@
 
 int main()
 {
-	
+	char x[] = { "I LOVE YOU!" };
+	int y;
+	scanf_s("%d", &y);
+	if (y == 1)
+	{
+		printf("%s\n", x);
+	}
+	else
+	{
+		printf("\n");
+	}
 	system("pause");
 	return 0;
 }
